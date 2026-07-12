adee1d5d6a8b9235e17920b29bd6a4d34934553a
GOKUL CHANDRA BHATT <142083386+Gokul-Bhatt@users.noreply.github.com>
2026-07-12 23:38:28 +0530

    contact
    working on contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,52 +1,143 @@
-import React, { useRef } from 'react'
-import "./Contact.css"
-import facebook from "../../assets/facebook-icon.png"
-import twitter from "../../assets/twitter.png"
-import insta from "../../assets/instagram.png"
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import "./Contact.css";
 
-
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const form = useRef();
+
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_3vrv0xd', 'template_ofbne8t', form.current, {
-        publicKey: 'fDytvibGMJPhxPCmz',
+      .sendForm(
+        "service_3vrv0xd",
+        "template_ofbne8t",
+        form.current,
+        "fDytvibGMJPhxPCmz"
+      )
+      .then(() => {
+        alert("Message Sent Successfully 🚀");
+        e.target.reset();
       })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-          e.target.reset();
-          alert('Email sent');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
+      .catch(() => {
+        alert("Something went wrong.");
+      });
   };
+
   return (
-    <section className="contactpage">
-      <div className="contact">
-        <h1 className="contactpageTitle">Contact Me</h1>
-        <span className="contactdecs">Please fill out the form below to discuss any work opportunities.</span>
-        <form className="contactForm" ref={form}  onSubmit={sendEmail}>
-          <input type="text" className="name" placeholder='Name' name='your_name' id='info' />
-          <input type="email" className="email" placeholder='Email' name='your_email'id='info' />
-          <textarea name="message" rows="5" placeholder='message' className='msg'id='info' ></textarea>
-          <button className="submitbtn" type='submit' value='send'>Submit</button>
-          <div className="links">
+    <section className="contact-section" id="contact">
 
-           <a href="https://www.instagram.com/"><img src={insta} alt="instagram" className="link"  /></a> 
-          <a href="https://twitter.com/i/flow/login?redirect_after_login=%2FGokul15891775"><img src={twitter} alt="twitter" className="link" /></a>  
-          <a href="https://www.facebook.com/">  <img src={facebook} alt="facebook" className="link" /></a> 
-          </div>
-        </form>
+      <div className="contact-heading">
+
+        <h2>
+          Get In <span>Touch</span>
+        </h2>
+
+        <p>
+          Have an idea or opportunity? Let's build something amazing together.
+        </p>
+
       </div>
-    </section>
-  )
-}
 
-export default Contact
+      <div className="contact-container">
+
+        {/* Left Side */}
+
+        <div className="contact-info">
+
+          <h3>Contact Information</h3>
+
+          <div className="info-card">
+            <h4>📧 Email</h4>
+            <p>gokulbhatt864@gmail.com</p>
+          </div>
+
+          <div className="info-card">
+            <h4>📍 Location</h4>
+            <p>Dehradun, Uttarakhand, India</p>
+          </div>
+
+          <div className="info-card">
+            <h4>💼 Available For</h4>
+            <p>Internships • Freelance • Full-Time</p>
+          </div>
+
+          <div className="socials">
+
+            <a
+              href="http://github.com/Gokul-Bhatt"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/gokul-bhatt/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+
+            <a
+              href="https://leetcode.com/u/Gokul-/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LeetCode
+            </a>
+
+          </div>
+
+        </div>
+
+        {/* Right Side */}
+
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="contact-form"
+        >
+
+          <input
+            type="text"
+            name="your_name"
+            placeholder="Your Name"
+            required
+          />
+
+          <input
+            type="email"
+            name="your_email"
+            placeholder="Your Email"
+            required
+          />
+
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+          />
+
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Write your message..."
+            required
+          ></textarea>
+
+          <button type="submit">
+            Send Message 🚀
+          </button>
+
+        </form>
+
+      </div>
+
+    </section>
+  );
+};
+
+export default Contact;
